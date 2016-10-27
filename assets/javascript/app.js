640bc03e8b82d010f47316213a1f106a5d1cdb03
Ryan House <Portuga@Ryans-MacBook-Pro.local>
2016-10-27 10:40:35 -0700

    Win/Lose logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,40 +2,38 @@
 var triviaArray = [
 {
 	question:"Where is Scotland?",
-	answer1:"Eastern Europe",
-	answer2:"Southern Europe",
-	answer3:"Part of Scandnavia",
-	answer4:"<span class='correctBtn'>Part of the United Kingdom",
-	correct:"Part of the United Kingdom"
+	answer1:"<span class='notBtn'>Eastern Europe",
+	answer2:"<span class='notBtn'>Southern Europe",
+	answer3:"<span class='correctBtn'>Part of the United Kingdom",
+	answer4:"<span class='notBtn'>Part of Scandnavia"
 },
 {
 	question:"Which country spells whisky with an 'e'?",
-	answer1:"Canada",
-	answer2:"Japan",
-	answer3:"<span class='correctBtn'>America",
-	answer4:"Scotland",
-	correct:"America"
+	answer1:"<span class='correctBtn'>America",
+	answer2:"<span class='notBtn'>Japan",
+	answer3:"<span class='notBtn'>Canada",
+	answer4:"<span class='notBtn'>Scotland"
 },
 {
-	question:"",
-	answer1:"",
-	answer2:"",
-	answer3:"",
-	answer4:""
+	question:"Which process is NOT part of making whisky?",
+	answer1:"<span class='notBtn'>Malting",
+	answer2:"<span class='notBtn'>Mashing",
+	answer3:"<span class='notBtn'>Fermentation",
+	answer4:"<span class='correctBtn'>All are processes"
 },
 {
-	question:"",
-	answer1:"",
-	answer2:"",
-	answer3:"",
-	answer4:""
+	question:"Which is not a regional style of Scotch whisky?",
+	answer1:"<span class='notBtn'>Highlands",
+	answer2:"<span class='correctBtn'>Midlands",
+	answer3:"<span class='notBtn'>Islay",
+	answer4:"<span class='notBtn'>Speyside"
 },
 {
-	question:"",
-	answer1:"",
-	answer2:"",
-	answer3:"",
-	answer4:""
+	question:"Why is older whisky more expensive?",
+	answer1:"<span class='correctBtn'>Evaportation of the whisky leaves less whisky",
+	answer2:"<span class='notBtn'>It tastes so darn good!",
+	answer3:"<span class='notBtn'>Because  BevMo says so",
+	answer4:"<span class='notBtn'>Scotish regulations say so"
 },
 ]
 
@@ -47,6 +45,10 @@ var currentOption4;
 var playerGuessed = false;
 var intervalNumber = 30;
 var counter = 0;
+var wrongPause;
+var correctPause;
+var correct = 0;
+var wrong = 0;
 
 function triviaSlide (){
 	for(var i = counter; i<triviaArray.length; i++){
@@ -68,25 +70,54 @@ function addTrivia(){
 	$(".option2").html(currentOption2)
 	$(".option3").html(currentOption3)
 	$(".option4").html(currentOption4)
+	$(".option4").addClass("correctBtn");
+}
+
+function slideChange(){
+	playerGuessed = false;
+	counter++;
+	buttonHide();
+	stop();
 }
 
 function correctAnswerSlide(){
 	$(".question").html("<h2>Correct!!</h2>");
-	buttonHide();
-	stop();
+	slideChange();
 }
 
 function wrongAnswerSlide(){
 	$(".question").html("<h2>Wrong!!</h2>");
-	buttonHide();
-	stop();
+	slideChange();
+}
+function winnerSlide(){
+	$(".question").html(
+		"<h1>You Win!!!!</h1>" +
+		"<p>Answered Correctly: " + correct + "</p>" +
+		"<p>Answered Incorrectly: " + wrong + "</p>" 
+		);
+}
+function loserSlide(){
+	$(".question").html(
+		"<h1>You lack winning skills</h1>" +
+		"<p>Answered Correctly: " + correct + "</p>" +
+		"<p>Answered Incorrectly: " + wrong + "</p>" 
+		);
 }
 
 function nextSlide() {
-	buttonShow();
-	triviaSlide();
-	addTrivia();
-	timeyMcTimerFace = setInterval(countDown, 1000);
+	if(counter < triviaArray.length) {
+		buttonShow();
+		triviaSlide();
+		addTrivia();
+		stop();
+		timeyMcTimerFace = setInterval(countDown, 1000);
+	}
+	else if(counter == triviaArray.length && correct > wrong){
+		winnerSlide();
+	}
+	else if(counter == triviaArray.length && correct < wrong){
+		loserSlide();
+	}
 }
 
 function buttonHide(){
@@ -113,35 +144,51 @@ function countDown(){
 }
 
 function stop(){
+	clearInterval(wrongPause);
+	clearInterval(correctPause);
 	clearInterval(timeyMcTimerFace);
+	timeyMcTimerFace = undefined;
 	intervalNumber = 30;
 	$(".timer").html(intervalNumber);
 }
 
-var timeyMcTimerFace = setInterval(countDown, 1000);
+var timeyMcTimerFace;
+
+// function endGame(){
+// 	if(counter == triviaArray.length) {
+// 		stop();
+// 		console.log("game is done.");
+// 	}
+// }
 
 // start of game and subsequent slide changes
 $(document).ready(function(){
 
-
 	$(".game-start").show();
 	$(".slide1").hide();
+	// $(".endOfGame").hide();
 
 	$(".startBtn").on("click", function(){
 		$(".game-start").hide();
 		$(".slide1").show();
 		triviaSlide();
 		addTrivia();
-		timeyMcTimerFace;
+		clearInterval(timeyMcTimerFace);
+		timeyMcTimerFace = setInterval(countDown,1000);
 		
 	});
 	$(".btn").on("click", ".correctBtn", function(){
-		playerGuessed = false;
-		counter++;
+		correct++;
 		correctAnswerSlide();
-		setInterval(nextSlide, 3000);
+		correctPause = setInterval(nextSlide, 3000);
 	});
-});
 
+	$(".btn").on("click", ".notBtn", function(){
+		wrong++;
+		wrongAnswerSlide();
+		wrongPause = setInterval(nextSlide, 3000);
+	});
+
+});
 
 
